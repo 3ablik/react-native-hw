ae4dfc65b022a3c0d5110d57835accd3c8fd5b0d
3ablik <3ablick@gmail.com>
2025-02-20 23:33:52 +0500

    Hw 1

--- App.js
+++ App.js
@@ -1,37 +1,130 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import SwitchComponent from "./components/switchComponents";
-import BackgroundImageComponent from "./components/backgroundImageComponents";
-import ActiveIndicatorComponent from "./components/activeIndicatorComponent";
-import AlertComponent from "./components/AlertComponent";
-import ScrollComponent from "./components/ScrollComponent";
-import TouchableHighlightComponent from "./components/TouchableHighlightComponent";
-import TouchableOpacityComponent from "./components/TouchableOpacityComponent";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <SwitchComponent />
-        <BackgroundImageComponent />
-        <ActiveIndicatorComponent />
-        <AlertComponent />
-        <ScrollComponent />
-        <TouchableHighlightComponent />
-        <TouchableOpacityComponent />
+  const [user, setUser] = useState({
+    name: "",
+    number: "",
+    email: "",
+  });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
 
-        <StatusBar style="auto" />
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Register</Text>
+      <View style={styles.formContainer}>
+        <View>
+          <Text style={styles.label}>Name:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Name"
+            placeholderTextColor="rgb(200, 200, 200)"
+            onChangeText={setName}
+            value={name}
+          />
+        </View>
+
+        <View>
+          <Text style={styles.label}>Number:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Number"
+            placeholderTextColor="rgb(200, 200, 200)"
+            onChangeText={setNumber}
+            value={number}
+          />
+        </View>
+
+        <View>
+          <Text style={styles.label}>Email:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            placeholderTextColor="rgb(200, 200, 200)"
+            onChangeText={setEmail}
+            value={email}
+          />
+        </View>
       </View>
-    </SafeAreaView>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setUser({ name, number, email })}
+      >
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+
+      <View style={styles.userInfo}>
+        <Text style={styles.userText}>Name: {user.name}</Text>
+        <Text style={styles.userText}>Number: {user.number}</Text>
+        <Text style={styles.userText}>Email: {user.email}</Text>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
+    marginTop: 50,
+    flex: 1,
+    backgroundColor: "#101012",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    color: "rgb(240, 240, 240)",
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "blue",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+  label: {
+    color: "rgb(240, 240, 240)",
+    marginBottom: 5,
+  },
+  formContainer: {
+    width: 300,
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: "rgb(60, 161, 255)",
+  },
+  userInfo: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  userText: {
+    color: "white",
+    fontSize: 16,
   },
 });
